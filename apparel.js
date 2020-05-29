f53d64214a64ac3252442d290a4a1b4fa5ccf10b
steven-j96 <54819489+steven-j96@users.noreply.github.com>
2020-05-29 15:21:01 +0100

    update
    - Added error icon to email field
    - improved responsitivity
    - changed errors from display/no-display to visible/hidden

--- apparel.js
+++ apparel.js
@@ -1,9 +1,10 @@
 var input = document.querySelector("#email");
 var response = document.querySelector("#response");
-
+var errorIcon = document.querySelector(".error-icon");
 input.addEventListener("invalid", responseValue);
 
 function responseValue(){
     input.classList.add("invalid");
-    response.classList.add("d-block");
+    response.classList.add("visible");
+    errorIcon.classList.add("visible");
 }
